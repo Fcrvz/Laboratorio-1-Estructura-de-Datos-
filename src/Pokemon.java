6a9649bae7b36791a0d34eb2ef70093c3d480360
Felipe <felipe.carvajal2@mail.udp.cl>
2026-08-21 19:48:47 -0400

    semana 1

---
+++ src/Pokemon.java
@@ -0,0 +1,73 @@
+public class Pokemon {
+
+    private int id;
+    private String name;
+    private String type1;
+    private int hp;
+    private int attack;
+    private int defense;
+    private int speed;
+    private int totalStats;
+
+    /**
+     * Constructor por defecto.
+     */
+    public Pokemon() {
+    }
+
+    /**
+     * Constructor que inicializa los atributos del Pokemon y calcula su totalStats.
+     */
+    public Pokemon(int id, String name, String type1, int hp, int attack, int defense, int speed){
+        this.id = id;
+        this.name = name;
+        this.type1 = type1;
+        this.hp = hp;
+        this.attack = attack;
+        this.defense = defense;
+        this.speed = speed;
+        recalcularTotalStats(); // Llamamos a un método interno para evitar repetir código
+    }
+
+    private void recalcularTotalStats() {
+        this.totalStats = this.hp + this.attack + this.defense + this.speed;
+    }
+
+    // Getters y Setters
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getType1() { return type1; }
+    public void setType1(String type1) { this.type1 = type1; }
+
+    public int getHp() { return hp; }
+    public void setHp(int hp) {
+        this.hp = hp;
+        recalcularTotalStats();
+    }
+
+    public int getAttack() { return attack; }
+    public void setAttack(int attack) {
+        this.attack = attack;
+        recalcularTotalStats();
+    }
+
+    public int getDefense() { return defense; }
+    public void setDefense(int defense) {
+        this.defense = defense;
+        recalcularTotalStats();
+    }
+
+    public int getSpeed() { return speed; }
+    public void setSpeed(int speed) {
+        this.speed = speed;
+        recalcularTotalStats();
+    }
+
+    public int getTotalStats() { return totalStats; }
+
+}
---
+++ src/PokemonDatabase.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import edu.princeton.cs.algs4.Merge;
+
+
+public class PokemonDatabase{
+
+    /**
+     * se declara la lista de pokemons y se inicializa
+     */
+    private ArrayList<Pokemon> pokemons = new ArrayList<>();
+
+    public PokemonDatabase(){}
+
+    public void ordenarPorAlgoritmo(String algoritmo, String atributo){
+
+    }
+
+    public ArrayList<Pokemon> sequentialSearch(String clave, String atributo){
+        return new ArrayList<>();
+    }
+
+    public ArrayList<Pokemon> binarySearch(String clave, String atributo){
+        return new ArrayList<>();
+    }
+
+
+
+}
+
+
+
