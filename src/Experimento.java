151a8e9a8ca94ed7383a3b01f5ae644ab4af63a4
Felipe <felipe.carvajal2@mail.udp.cl>
2026-08-21 23:17:33 -0400

    Semana 1 completa: Javadocs listos y librerias agregadas

--- src/Experimento.java
+++ src/Experimento.java
@@ -1,2 +1,26 @@
+import java.util.*;
+import edu.princeton.cs.algs4.*;
+
+/**
+* Clase principal del laboratorio.
+* para ejecutar los experimentos de busqueda y orden, para luego
+* medir y exportar los tiempos de cada algoritmo.
+* Los datos de los archivos CSV se guardaran con los siguientes nombres
+* Ordenamiento: "instancia, selectionSort, mergeSort.
+* Busqueda: "instancia, nombre, t_ordenamiento, t_lineal, t_binaria.
+*/
+
 public class Experimento {
+
+    /**
+     * Por ahora se crea un objeto llamado prueba para comprobar
+     * la compatibilidad de las clases.
+     */
+
+    public static void main(String[] args){
+        
+        //objeto de prueba para comprobar posibles errores
+        PokemonDatabase prueba = new PokemonDatabase();
+
+    }
 }
---
+++ src/PokemonGenerator.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import edu.princeton.cs.algs4.*;
+
+public class PokemonGenerator {
+
+    public static ArrayList<Pokemon> generateDatabase(int n, long seed){
+
+        StdRandom.setSeed(seed);
+
+        //se retorna un ArrayList vacio para comprobar posibles errores
+        return new ArrayList<>();
+
+    }
+
+}
